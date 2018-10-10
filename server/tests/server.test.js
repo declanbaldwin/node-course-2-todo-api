a6b8514153e9e1d16505d9bdb80bf8c40e79dc9f
Declan Baldwin <dbaldwin4@boxfusionconsulting.com>
2018-10-10 08:48:51 +0100

    Add routes

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,9 +1,14 @@
 const expect = require('expect');
 const request = require('supertest');
 
-const {app} = require('./..server');
-const {Todo} = require('./..models/todo');
+const {app} = require("../server.js");
+const {Todo} = require("../models/todo.js");
 
+beforeEach((done) => {
+    Todo.remove({}).then(() => {
+        done();
+    });
+});
 
 describe('POST /todos', () => {
     it('should create a new todo', (done) => {
@@ -31,5 +36,24 @@ describe('POST /todos', () => {
                     done(error);
                 });
             });
-    })
+    });
+
+    it('should not create a todo with invalid body data', (done) => {
+        request(app)
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((error, response) => {
+                if(error) {
+                    return done(error);
+                }
+
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(0);
+                    done();
+                }).catch(error => {
+                    done(error);
+                });
+            });
+    });
 });
